0268cff205e7ff1447aab3c00ead66750b5615a8
Forzani Aurelie <aforzani@localhost.localdomain>
2017-10-19 11:37:13 +0200

    recherche du flot max a ajouter

--- Amelioration.m
+++ Amelioration.m
@@ -3,15 +3,28 @@ function [ flots ] = Amelioration( puits, source , sommets, marques, pointeurs, 
 %   Detailed explanation goes here
 
 t = puits;
+%% Trouver la quantité maximale à ajouter au flot
+minFlot = INF;
+while( t ~= source)
+    s = marques(t);
+    if (marque(t) > 0)
+        indArc = getIndArc(s, t, pointeurs, successeurs, sommets);
+    elseif (marque(t) < 0)
+        indArc = getIndArc(t, -s, pointeurs, successeurs, sommets);
+    end
+    minFlot = min(minFlot, abs(flots(indArc));
+    t = abs(s);
+end
+
 while( t ~= source)
     if (marque(getIndSommet(t)) > 0)
-        s = marques(getIndSommet(t));
+        s = marques(t);
         indArc = getIndArc(s, t, pointeurs, successeurs, sommets);
-        flots(indArc) = flots(indArc) + 1;
-    else if (marque(getIndSommet(t)) < 0)
-            s = marques(getIndSommet(t));
-        indArc = getIndArc(t, -s, pointeurs, successeurs, sommets);
-        flots(indArc) = flots(indArc) - 1;
+        flots(indArc) = flots(indArc) + minFlot;
+    else if (marque(t) < 0)
+            s = marques(t);
+            indArc = getIndArc(t, -s, pointeurs, successeurs, sommets);
+            flots(indArc) = flots(indArc) - minFlot;
         end
         t = s;
     end
