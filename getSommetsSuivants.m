14ad3b3dbac066d50f38c4ca3f488f7ec5b79a5e
Elisabelar Marie <melisabe@localhost.localdomain>
2017-10-19 14:40:42 +0200

    ajout commentaires

--- getSommetsSuivants.m
+++ getSommetsSuivants.m
@@ -2,21 +2,25 @@ function[SommetsSuivants] = getSommetsSuivants(Pointeur,Successeurs,Sommet)
 % retourne la liste des sommets atteignables directement par Sommet
 
 %% EN ENTREE
-%  
-%
+% Pointeur : liste du nombre d'arcs sortant par sommet
+% Successeurs : listte des successeurs de chaque sommet
+% Sommet : sommet dont on cherche les successeurs
+
 %% EN SORTIE
 %  x : vecteur des valeurs des variables maximisant la valeur du critère.
 %  F : valeur optimale du critere.
 %% DEBUT DU PROGRAMME
 SommetsSuivants = [];
 
+%indices du premier et du dernier arc de Sommet
 indSucc = getIndSucc(Pointeur,Sommet);
 
-i = indSucc(1);
-j = indSucc(2);
+debutArc = indSucc(1);
+finArc = indSucc(2);
 
 ind = 1;
-for k = i:j
+%récupérer les Sommets suivants
+for k = debutArc:finArc
     SommetsSuivants(ind) = Successeurs(k);
     ind = ind + 1;
 end
