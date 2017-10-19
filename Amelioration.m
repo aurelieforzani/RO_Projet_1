fdd1156c4f3df45abb7d087a16655e87f06ddf98
Elisabelar Marie <melisabe@localhost.localdomain>
2017-10-19 16:59:33 +0200

    commentaire amelioration

--- Amelioration.m
+++ Amelioration.m
@@ -1,32 +1,54 @@
 function [ flots ] = Amelioration( puits, source , sommets, marques, pointeurs, successeurs, flots)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+%% Améliore le flot compatible courant avec le marquage spécifié
+%% EN ENTREE :
+% puits : sommet d'arrivée du graphe
+% source : sommet de départ du graphe
+% sommets : Les sommets du graphe
+% marques : marquage permettant d'améliorer le flot
+% pointeurs : tableau du nombre d'arcs sortant de chaque sommet
+% successeurs : tableau des sommets pointés par des arcs
+% flots : flot associé à chaque arc
+
+%% EN SORTIE 
+% flots : flots améliorés
 
 t = puits;
 %% Trouver la quantité maximale à ajouter au flot
 minFlot = inf;
 while( t ~= source)
+    
+    % s : sommet précédent t dans le marquage, pondéré par le sens de l'arc
+    % les reliant
     s = marques(t);
+    
+    % Recherche de l'indice de l'arc liant s et t
+    % arc orienté positivement
     if (s > 0)
         
         indArc = getIndArc(s, t, pointeurs, successeurs);
+    % arc orienté négativement
     else
         
         indArc = getIndArc(t, -s, pointeurs, successeurs);
     end
+    
+    % calcul du nouveau flot maximal à ajouter
     if (s ~= 0)
         minFlot = min(minFlot, abs(flots(indArc)));
     end
     t = abs(s);
 end
 
+%% Affecter la nouvelle valeur du flot à chaque arc 
 t = puits;
 while( t ~= source)
+    
+    % arc orienté négativement
     if (marques(sommets(t)) > 0)
-        %if (marque(getIndSommet(t)) > 0)
         s = marques(t);
         indArc = getIndArc(s, t, pointeurs, successeurs);
         flots(indArc) = flots(indArc) + minFlot;
+    % arc orienté négativement
     elseif (marques(t) < 0)
         s = marques(t);
         indArc = getIndArc(t, -s, pointeurs, successeurs);
